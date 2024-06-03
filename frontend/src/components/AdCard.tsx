a89877ff039df8fbdc40da99d6ef3ced576692ae
Florence Buchelet <florence.buchelet@gmail.com>
2024-06-03 16:33:25 +0200

    added basic front with nextJS

---
+++ frontend/src/components/AdCard.tsx
@@ -0,0 +1,23 @@
+export type AdCardProp = {
+    imgUrl: string;
+    title: string;
+    price: number;
+    link: string;
+};
+
+function AdCard({ imgUrl, title, price, link}: AdCardProp) {
+
+    return (
+        <div className="ad-card-container">
+            <a className="ad-card-link" href={link}>
+                <img className="ad-card-image" src={imgUrl} />
+                <div className="ad-card-text">
+                    <div className="ad-card-title">{title}</div>
+                    <div className="ad-card-price">{price} €</div>
+                </div>
+            </a>
+        </div>
+    );
+}
+
+export default AdCard;
