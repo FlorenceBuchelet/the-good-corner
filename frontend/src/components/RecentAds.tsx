d4f11d20be7351b5344e4fe4a6f1328b74f8003f
Florence Buchelet <florence.buchelet@gmail.com>
2024-06-04 10:16:35 +0200

    price count

--- frontend/src/components/RecentAds.tsx
+++ frontend/src/components/RecentAds.tsx
@@ -1,38 +1,40 @@
 import { log } from "console";
 import AdCard, { AdCardProp } from "./AdCard";
+import { useState } from "react";
 
 
 function RecentAds() {
+    const [total, setTotal] = useState(0);
 
     const ads: AdCardProp[] = [
         {
             imgUrl: "/images/table.webp",
-            link: "/ads/table",
+            link: "/ads/1",
             price: 120,
             title: "table",
         }, {
             imgUrl: "/images/dame-jeanne.webp",
-            link: "/ads/dame-jeanne",
+            link: "/ads/2",
             price: 75,
             title: "Dame Jeanne",
         }, {
             imgUrl: "/images/vide-poche.webp",
-            link: "/ads/vide-poche",
+            link: "/ads/3",
             price: 4,
             title: "Vide poche",
         }, {
             imgUrl: "/images/vaisselier.webp",
-            link: "/ads/vaisselier",
+            link: "/ads/4",
             price: 900,
             title: "Vaisselier",
         }, {
             imgUrl: "/images/bougie.webp",
-            link: "/ads/bougie",
+            link: "/ads/5",
             price: 8,
             title: "Bougie",
         }, {
             imgUrl: "/images/porte-magazine.webp",
-            link: "/ads/porte-magazine",
+            link: "/ads/6",
             price: 45,
             title: "Porte Magazine",
         }
@@ -40,15 +42,25 @@ function RecentAds() {
     return (
         <>
             <h2>Annonces récentes</h2>
+            <p>Prix total : {total} €</p>
             <section className="recent-ads">
                 {ads.map((ad, index) => (
-                    <AdCard
-                        imgUrl={ad.imgUrl}
-                        link={ad.link}
-                        price={ad.price}
-                        title={ad.title}
+                    <article
                         key={ad.title}
-                    />
+                    >
+                        <AdCard
+                            imgUrl={ad.imgUrl}
+                            link={ad.link}
+                            price={ad.price}
+                            title={ad.title}
+                        />
+                        <button
+                            className="button"
+                            onClick={() => setTotal(total + ad.price)}
+                        >
+                            Add price to total
+                        </button>
+                    </article>
                 ))}
             </section>
         </>
